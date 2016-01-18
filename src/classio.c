c9b769e7b97e1ce9565033dfe523bb31574788ed
mzogheib <mzogheib@users.noreply.github.com>
2016-01-18 18:12:43 +1100

    Added more comments

--- src/classio.c
+++ src/classio.c
@@ -8,13 +8,12 @@ static TextLayer *s_connection_layer;
 
 // Function runs every time the battery state changes
 static void handle_battery(BatteryChargeState charge_state) {
-  static char battery_text[] = "100% charged";
 
   if (charge_state.is_charging) {
-		// Display something indicating that watch is charging
+	// Display something indicating that watch is charging
 		
   } else {
-		// Display the charge percentage
+	// Display the charge percentage
 
   }
 
@@ -22,8 +21,6 @@ static void handle_battery(BatteryChargeState charge_state) {
 
 // Function runs per time tick, e.g. second, minute, hour, which is passed in via 'units_changed'
 static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
-  // Needs to be static because it's used by the system later.
-  static char s_time_text[] = "00:00:00";
 
 }
 
@@ -32,14 +29,14 @@ static void handle_bluetooth(bool connected) {
 
 }
 
+// Function runs when the window is pushed onto the stack, e.g. app load
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
 
 	// Create and initialize the text layers for time, connection and battery
 
-  // Display the time immediately on start up (will break if NULL tick event accessed).
-  // (This is why it's a good idea to have a separate routine to do the update itself.)
+  // Display the time, connection and battery state immediately on start up
  
 	// Subscribe to the time service
 	
@@ -49,10 +46,9 @@ static void main_window_load(Window *window) {
 	
 	// Add all text layers to the window layer
 	
-	// Check battery state
- 
 }
 
+// Function runs when the window is popped off the stack, e.g. app exit
 static void main_window_unload(Window *window) {
 	// Unsubscribe from system events
 	
@@ -60,11 +56,12 @@ static void main_window_unload(Window *window) {
 
 }
 
+// All functions below are typically unchanged for simple apps and watchfaces.
 static void init() {
   s_main_window = window_create();
   window_set_background_color(s_main_window, GColorBlack);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
+    .load = main_window_load,	
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
